eeba171695a9d326d632bf63d423347628defa33
braddown <brad@kudosity.com>
2025-04-22 17:25:40 +1000

    Initial Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,10 @@
- 
+import { redirect } from 'next/navigation';
+
+// This is a Server Component
+export default function Home() {
+  // This is a server-side redirect in Next.js App Router
+  redirect('/login');
+  
+  // This line is never reached due to the redirect
+  return null;
+} 
